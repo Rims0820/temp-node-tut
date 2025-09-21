157104a26320035a99055e3da1d2462b8b1b6a73
Your Name <your@email.com>
2025-09-21 08:08:39 +0530

    first commit

---
+++ 6-modules.js
@@ -0,0 +1,7 @@
+const names = require('./2-names')
+const sayHi = require('./3-utils')
+const data = require('./4-alternative-flavor')
+require('./5-mind-grenade')
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
